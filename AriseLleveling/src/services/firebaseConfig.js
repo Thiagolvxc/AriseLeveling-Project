9d20145323d08546ad26350fe64352af56c29f5e
Thiagolvxc <santiago.loaizava@gmail.com>
2025-09-29 22:57:42 -0500

    Registro en Firebase y creacion del .env

---
+++ AriseLleveling/src/services/firebaseConfig.js
@@ -0,0 +1,23 @@
+import React from "react"
+import {initializeApp} from "firebase/app"
+import {initializeAuth, getReactNativePersistence} from "firebase/auth"
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
+import {getStorage} from "firebase/storage"
+
+const firebaseConfig = {
+    apiKey: process.env.EXPO_PUBLIC_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLICA_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+const storage = getStorage(app);
+
+export {auth, storage};
